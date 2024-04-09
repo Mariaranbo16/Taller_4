4bab29f6bee80165581271fe1005a7d1e5f068de
Mariana Arango <marangob@udistrital.edu.co>
2024-04-09 14:52:14 -0500

    Version1

---
+++ Programa2.py
@@ -0,0 +1,13 @@
+# Importar la biblioteca 
+# Hacer un ALIAS para no escribir completo. (np)  ES OPCIONAL
+
+import numpy as np
+
+# Crear arreglos de dos  dimensiones a partir de listas
+
+miLista = [[1,2,3,4],[5,6,7,8],[9,10,11,20]]
+
+miArreglo = np.array(miLista)
+print(miArreglo.ndim)
+print(miArreglo)
+print(miLista)
