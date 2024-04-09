b0cb1a146f171b64a8ff848e3e1c4a01e65406ce
Mariana Arango <marangob@udistrital.edu.co>
2024-04-09 15:19:37 -0500

    Version0.3

--- Programa.py
+++ Programa.py
@@ -1,18 +1,17 @@
 # Importar la biblioteca 
-# Hacer un ALIAS para no escribir completo. (np)  ES OPCIONAL
-
 import numpy as np
 
-# Una forma de crear arreglos ndarray es usando una lista 
-
-miLista = [3,5,7,9]
-miArreglo = np.array (miLista)
-
-# Atributos de los arreglos 
+# Usando funciones de "relleno" de arreglos 
+miArreglo = np.arange(-10,10)
 print (miArreglo.ndim)
-print (miArreglo.shape)
-print (miArreglo.size)
-print (miArreglo.dtype)
 
-# No se puede olvidar poner las llaves en la lista de numpy
-miArreglo2 = np.array ([2,6,7,90])
+miArreglo.reshape((2,10))
+
+miArreglo = np.ones ((5,4))
+print (miArreglo)
+
+miArreglo = np.zeros ((4,5))
+print (miArreglo)
+
+miArreglo = np.empty ((4,5))
+print (miArreglo)
