c25cc6ebbfe953f59074d839acd5b2a08a3cb707
Mariana Arango <marangob@udistrital.edu.co>
2024-04-09 15:06:01 -0500

    Version0.2

--- Programa2.py
+++ Programa2.py
@@ -7,7 +7,7 @@ import numpy as np
 
 miLista = [[1,2,3,4],[5,6,7,8],[9,10,11,20]]
 
-miArreglo = np.array(miLista)
+miArreglo = np.array(miLista, dtype = int)
 print(miArreglo.ndim)
 print(miArreglo)
 print(miLista)
