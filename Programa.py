a9bc75f56d26a555a76c124d7dc53d7b663f6513
Mariana Arango <marangob@udistrital.edu.co>
2024-04-10 14:32:09 -0500

    Versión0.7_Un arreglo

--- Programa.py
+++ Programa.py
@@ -2,28 +2,12 @@
 import numpy as np
 
 a =  np.array ([5,10,15,20,25])
-print (a)
-
-b = np.array ([3,4,7,9,56])
-print (b)
-
-c = a-b
+# ndarray
+c = a.sum()
 print (c)
 
-c = a + b
+c = a.min()
 print (c)
 
-c = a * b
-print (c)
-
-c = a**b  # Es el elevado
-print (c)
-
-c = np.sin (b)
-print (c)
-
-c = (b>6)
-print (c)
-
-c = a @ b  # Es el producto punto 
+c = a.mean()
 print (c)
